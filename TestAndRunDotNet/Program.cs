d81a942e58fac1728aebe704ac37627443aa8cc1
VaishnaviI <yshnav003@gmail.com>
2026-05-21 23:15:02 +0530

    Integrate species stats retrieval in Program.cs
    Added functionality to get species statistics and return as JSON.

--- TestAndRunDotNet/Program.cs
+++ TestAndRunDotNet/Program.cs
@@ -4,20 +4,22 @@ using System.Collections.Generic;
 using System.Collections.Concurrent;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
-;
+using QueriedData;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 var fileReader = new FileReader();
-// var irises = fileReader.GetIrises();
-// Console.WriteLine($"Number of rows: {irises.Count}");
+var irisQuery = new IrisQueries();
+
+var getSpeciesStats = irisQuery.GetStatsPerSpecies();
+
 
 var top10Countries = (await fileReader.GetCountries()).Take(10).ToList();
 
 var countryNames = string.Join("\n",top10Countries.Select(country => country.Name.Common));
 
-app.MapGet("/", () => $"{countryNames}");
+app.MapGet("/", () => $"{JsonSerializer.Serialize(getSpeciesStats)}");
 
 app.Run();
 
